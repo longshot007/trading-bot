5ca14a587def7c2bd1d294859dca59f0f1839843
rcughan77-creator <rcughan77@gmail.com>
2026-03-13 17:47:10 -0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,7 +4,7 @@ import pandas as pd
 
 import os
 
-API_KEY = os.environ.get("ALPACA_API_KEY")
+API_KEY = os.environ.get("ALPACA_API_KEY_ID")
 SECRET_KEY = os.environ.get("ALPACA_SECRET_KEY")
 
 BASE_URL = "https://paper-api.alpaca.markets"
